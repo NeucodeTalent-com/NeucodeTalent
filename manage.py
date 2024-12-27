67322035e3cae52f489067364e872fa9d57f2638
NeucodeTalent-com <anantsol@neucodetalent.com>
2024-12-27 15:58:48 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'NFS_360.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'NFS_360.settings'
+    settings_module = 'NFS_360.deployment' if 'Hostname_Neucode' in os.environ else 'NFS_360.settings'
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
